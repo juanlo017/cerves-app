121c87d8fcab144783c43eaab531d2032b58ce19
Juan López Quirós <jualopqui1@alum.us.es>
2026-02-08 23:56:44 +0100

    feat: Arreglados botones tabs

--- cerves-app/app/(tabs)/_layout.tsx
+++ cerves-app/app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 import { useColorScheme } from '@/components/useColorScheme';
 import { Theme } from '@/constants/Theme';
@@ -30,24 +30,36 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'SEMANAL',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="add"
         options={{
           title: 'AÑADIR',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle" size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="calendar"
         options={{
           title: 'CALENDARIO',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="scoreboard"
         options={{
           title: 'RANKING',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="trophy" size={size} color={color} />
+          ),
         }}
       />
     </Tabs>
